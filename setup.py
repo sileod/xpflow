fedd9bb55fa5ee3696305530d54bf3641d0687a6
sileod <damien.sileo@gmail.com>
2021-09-02 17:50:18 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='xpflow',
       version='0.1',
       description='Utilities for representing experiments with classes',
@@ -7,4 +9,7 @@ setup(name='xpflow',
       author='sileod',
       license='MIT',
       py_modules=['xpflow'],
+      install_requires=['easydict'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       zip_safe=False)
