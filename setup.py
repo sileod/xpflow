c05a7cd299dcae61acd4fabcd61568b6e099def9
sileod <damien.sileo@gmail.com>
2021-10-28 14:51:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='xpflow',
-      version='0.1',
+      version='0.2',
       description='Utilities for representing experiments with classes',
       url='https://github.com/sileod/xpflow',
       author='sileod',
