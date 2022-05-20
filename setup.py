caab2988b7a3b1ac82ec414b7f429f5cae5018be
sileod <damien.sileo@gmail.com>
2022-05-20 15:38:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='xpflow',
-      version='0.2',
+      version='0.3',
       description='Utilities for representing experiments with classes',
       url='https://github.com/sileod/xpflow',
       author='sileod',
       license='MIT',
       install_requires=['easydict'],
-      download_url='https://github.com/sileod/xpflow/archive/refs/tags/v0.tar.gz',
+      download_url='https://github.com/sileod/xpflow/archive/refs/tags/v0.3.tar.gz',
       py_modules=['xpflow'],
       long_description=long_description,
       long_description_content_type='text/markdown',
